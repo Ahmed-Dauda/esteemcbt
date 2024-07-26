1a3787fb3d4d32350683bf5b2a406c1c6ea63a58
Ahmed-Dauda <addatainsight@gmail.com>
2024-07-26 17:38:04 +0100

    updating see not start exam subjects

--- sms/views.py
+++ sms/views.py
@@ -416,7 +416,7 @@ class DigitalForm(ListView):
 
 
 
- # Cache for 15 minutes
+@method_decorator(cache_page(60 * 40), name='dispatch')  # Cache for 15 minutes
 class Homepage(ListView):
     model = Courses
     template_name = 'sms/dashboard/homepage1.html'
