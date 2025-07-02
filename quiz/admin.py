97ebe912acfe66d6812d87642e7353f1693918d2
Ahmed-Dauda <addatainsight@gmail.com>
2025-07-02 12:26:26 +0100

    Trigger rebuild

--- quiz/admin.py
+++ quiz/admin.py
@@ -189,7 +189,7 @@ class ResultAdmin(ImportExportModelAdmin, ExportActionMixin):
     resource_class = ResultResource
     list_display = ['student', 'exam','schools', 'marks', 'exam_type', 'result_class', 'session', 'term', 'created']
     list_filter = ['exam', 'student', 'exam_type', 'session', 'term', 'created']
-    search_fields = ['student__first_name', 'student__last_name', 'exam__course_name__title', 'created']
+    search_fields = ['schools','student__first_name', 'student__last_name', 'exam__course_name__title', 'created']
     ordering = ['exam__course_name']
  
 admin.site.register(Result, ResultAdmin)
