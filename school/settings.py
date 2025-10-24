686f0aaa342b9e2d3044442a82c128b18ed6b3e9
Ahmed-Dauda <addatainsight@gmail.com>
2025-10-24 03:37:06 +0100

    Fix: added missing hitcount migration

--- school/settings.py
+++ school/settings.py
@@ -49,9 +49,9 @@ INSTALLED_APPS = [
     'hitcount',
     'crispy_forms',
     'crispy_bootstrap5',
-    # 'allauth',
-    # 'allauth.account',
-    # 'allauth.socialaccount',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary',
     'django_select2',
     'embed_video',
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     'django_mathjax',
 
 
-    # 'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.google',
     # 'allauth.socialaccount.providers.twitter',
 ]
 
