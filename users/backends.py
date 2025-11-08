e4faa3e84d8540b2fa62e7b5253e278d7948bf9f
Ahmed-Dauda <addatainsight@gmail.com>
2025-11-08 14:27:25 +0100

    Fix: added missing hitcount migration

--- users/backends.py
+++ users/backends.py
@@ -1,12 +1,22 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.backends import ModelBackend
 
 User = get_user_model()
 
-class UsernameOnlyBackend(ModelBackend):
+class UsernameOnlyBackend:
     def authenticate(self, request, username=None, password=None, **kwargs):
+        if not username:
+            return None
         try:
             user = User.objects.get(username=username)
-            return user if user.is_active else None
+        except User.DoesNotExist:
+            return None
+        # Optional: block staff/admins
+        if user.is_staff or user.is_superuser:
+            return None
+        return user
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
