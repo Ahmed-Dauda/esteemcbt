ac3e5c085879444e3df8d89ed7f713b53a0513db
Ahmed-Dauda <addatainsight@gmail.com>
2026-02-06 17:21:07 +0100

    Change cleanup to 2 hours

--- quiz/management/commands/cleanup_old_sessions.py
+++ quiz/management/commands/cleanup_old_sessions.py
@@ -2,7 +2,7 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from datetime import timedelta
-from quiz.models import StudentExamSession
+from quiz.models import StudentExamSession,Result
 
 class Command(BaseCommand):
     help = 'Delete StudentExamSession records older than 2 hours'
@@ -12,11 +12,16 @@ class Command(BaseCommand):
         deleted_count, _ = StudentExamSession.objects.filter(
             created__lt=two_hours_ago
         ).delete()
+
+        thirty_days_ago = timezone.now() - timedelta(days=30)
+        deleted_results, _ = Result.objects.filter(
+            created__lt=thirty_days_ago
+        ).delete()
         
         self.stdout.write(
-            self.style.SUCCESS(f'Deleted {deleted_count} old sessions')
+            self.style.SUCCESS(f'Deleted {deleted_count} old sessions and {deleted_results} old results')
         )
-        
+
 # class Command(BaseCommand):
 #     help = 'Delete StudentExamSession records older than 1 minute'
 
