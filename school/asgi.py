f549dad6cc81e6c06917957f2e1a0155cb1e0838
Ahmed-Dauda <addatainsight@gmail.com>
2026-05-13 00:21:12 +0100

    fix psycopg2 build error

--- school/asgi.py
+++ school/asgi.py
@@ -1,15 +1,36 @@
-"""
-ASGI config for school project.
+# """
+# ASGI config for school project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+# It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+# """
 
-# import os
+# # import os
+# # from dotenv import load_dotenv
+# # load_dotenv(dotenv_path=".env", override=True)
+
+# # from django.core.asgi import get_asgi_application
+
+# # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school.settings')
+
+# # application = get_asgi_application()
+
+# """
+# ASGI config for school project.
+
+# It exposes the ASGI callable as a module-level variable named ``application``.
+
+# For more information on this file, see
+# https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+# """
+
 # from dotenv import load_dotenv
 # load_dotenv(dotenv_path=".env", override=True)
+
+
+# import os
 
 # from django.core.asgi import get_asgi_application
 
@@ -17,25 +38,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 # application = get_asgi_application()
 
-"""
-ASGI config for school project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
-from dotenv import load_dotenv
-load_dotenv(dotenv_path=".env", override=True)
-
-
 import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="/app/.env", override=True)
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school.settings')
 
 application = get_asgi_application()
-
 
