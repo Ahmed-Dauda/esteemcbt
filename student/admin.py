456410236266ebbc0dcdd6f32e6aab4301a49628
Ahmed-Dauda <addatainsight@gmail.com>
2026-05-30 12:58:06 +0100

    test deployment

--- student/admin.py
+++ student/admin.py
@@ -1,4 +1,4 @@
-import importlibortlib
+import importlib
 from django.contrib import admin
 from student.models import (Payment,PDFDocument,Clubs,
                             PDFGallery,Directors, Management,
