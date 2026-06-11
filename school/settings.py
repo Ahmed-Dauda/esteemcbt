7a9526d5decf591780c1e6d68158ba5a36911f47
Ahmed-Dauda <addatainsight@gmail.com>
2026-06-11 20:16:09 +0100

    fix: add cloudinary config initialization to settings

--- school/settings.py
+++ school/settings.py
@@ -189,6 +189,12 @@ SOCIALACCOUNT_PROVIDERS = {
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+)
 
 
 # import cloudinary
